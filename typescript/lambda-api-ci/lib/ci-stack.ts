b938524ef938d141c82614ae52a42ceb9285b4de
Harry Solovay <harrysolovay@gmail.com>
2020-11-09 15:38:13 -0500

    formatting fixes

--- typescript/lambda-api-ci/lib/ci-stack.ts
+++ typescript/lambda-api-ci/lib/ci-stack.ts
@@ -1,123 +1,126 @@
-import { CodeCommitSourceAction, CodeBuildAction } from "@aws-cdk/aws-codepipeline-actions"
-import { PolicyStatement } from "@aws-cdk/aws-iam"
-import { Construct, Stack, StackProps } from "@aws-cdk/core"
-import { PipelineProject, LinuxBuildImage } from "@aws-cdk/aws-codebuild"
-import { Artifact, Pipeline } from "@aws-cdk/aws-codepipeline"
-import { Repository } from "@aws-cdk/aws-codecommit"
-import { lambdaApiStackName, lambdaFunctionName } from "../bin/lambda"
-import { C$ } from "@crosshatch/cdk"
+import {
+  CodeCommitSourceAction,
+  CodeBuildAction,
+} from "@aws-cdk/aws-codepipeline-actions";
+import { PolicyStatement } from "@aws-cdk/aws-iam";
+import { Construct, Stack, StackProps } from "@aws-cdk/core";
+import { PipelineProject, LinuxBuildImage } from "@aws-cdk/aws-codebuild";
+import { Artifact, Pipeline } from "@aws-cdk/aws-codepipeline";
+import { Repository } from "@aws-cdk/aws-codecommit";
+import { lambdaApiStackName, lambdaFunctionName } from "../bin/lambda";
+import { C$ } from "@crosshatch/cdk";
 
 interface CIStackProps extends StackProps {
-    repositoryName: string
+  repositoryName: string;
 }
 
 export const CIStack = C$(
-    Stack,
-    (def, props: CIStackProps) => {
-        const pipeline = def`Pipeline`(Pipeline)
+  Stack,
+  (def, props: CIStackProps) => {
+    const pipeline = def`Pipeline`(Pipeline);
 
-        const repo = Repository.fromRepositoryName(
-            def.scope,
-            "WidgetsServiceRepository",
-            props.repositoryName
-        )
+    const repo = Repository.fromRepositoryName(
+      def.scope,
+      "WidgetsServiceRepository",
+      props.repositoryName
+    );
 
-        const sourceOutput = new Artifact("SourceOutput")
-        const sourceAction = new CodeCommitSourceAction({
-            actionName: "CodeCommit",
-            repository: repo,
-            output: sourceOutput,
-        })
+    const sourceOutput = new Artifact("SourceOutput");
+    const sourceAction = new CodeCommitSourceAction({
+      actionName: "CodeCommit",
+      repository: repo,
+      output: sourceOutput,
+    });
 
-        pipeline.addStage({
-            stageName: "Source",
-            actions: [sourceAction],
-        })
+    pipeline.addStage({
+      stageName: "Source",
+      actions: [sourceAction],
+    });
 
-        const createBuildStage = (pipeline: Pipeline, sourceOutput: Artifact) => {
-            const project = def`BuildProject`(PipelineProject, {
-                environment: {
-                    buildImage: LinuxBuildImage.STANDARD_3_0,
-                },
-            })
+    const createBuildStage = (pipeline: Pipeline, sourceOutput: Artifact) => {
+      const project = def`BuildProject`(PipelineProject, {
+        environment: {
+          buildImage: LinuxBuildImage.STANDARD_3_0,
+        },
+      });
 
-            const cdkDeployPolicy = new PolicyStatement()
-            cdkDeployPolicy.addActions(
-                "cloudformation:GetTemplate",
-                "cloudformation:CreateChangeSet",
-                "cloudformation:DescribeChangeSet",
-                "cloudformation:ExecuteChangeSet",
-                "cloudformation:DescribeStackEvents",
-                "cloudformation:DeleteChangeSet",
-                "cloudformation:DescribeStacks",
-                "s3:*Object",
-                "s3:ListBucket",
-                "s3:getBucketLocation",
-                "lambda:UpdateFunctionCode",
-                "lambda:GetFunction",
-                "lambda:CreateFunction",
-                "lambda:DeleteFunction",
-                "lambda:GetFunctionConfiguration",
-                "lambda:AddPermission",
-                "lambda:RemovePermission"
-            )
-            cdkDeployPolicy.addResources(
-                def.scope.formatArn({
-                    service: "cloudformation",
-                    resource: "stack",
-                    resourceName: "CDKToolkit/*",
-                }),
-                def.scope.formatArn({
-                    service: "cloudformation",
-                    resource: "stack",
-                    resourceName: `${lambdaApiStackName}/*`,
-                }),
-                def.scope.formatArn({
-                    service: "lambda",
-                    resource: "function",
-                    sep: ":",
-                    resourceName: lambdaFunctionName,
-                }),
-                "arn:aws:s3:::cdktoolkit-stagingbucket-*"
-            )
-            const editOrCreateLambdaDependencies = new PolicyStatement()
-            editOrCreateLambdaDependencies.addActions(
-                "iam:GetRole",
-                "iam:PassRole",
-                "iam:CreateRole",
-                "iam:AttachRolePolicy",
-                "iam:PutRolePolicy",
-                "apigateway:GET",
-                "apigateway:DELETE",
-                "apigateway:PUT",
-                "apigateway:POST",
-                "apigateway:PATCH",
-                "s3:CreateBucket",
-                "s3:PutBucketTagging"
-            )
-            editOrCreateLambdaDependencies.addResources("*")
-            project.addToRolePolicy(cdkDeployPolicy)
-            project.addToRolePolicy(editOrCreateLambdaDependencies)
+      const cdkDeployPolicy = new PolicyStatement();
+      cdkDeployPolicy.addActions(
+        "cloudformation:GetTemplate",
+        "cloudformation:CreateChangeSet",
+        "cloudformation:DescribeChangeSet",
+        "cloudformation:ExecuteChangeSet",
+        "cloudformation:DescribeStackEvents",
+        "cloudformation:DeleteChangeSet",
+        "cloudformation:DescribeStacks",
+        "s3:*Object",
+        "s3:ListBucket",
+        "s3:getBucketLocation",
+        "lambda:UpdateFunctionCode",
+        "lambda:GetFunction",
+        "lambda:CreateFunction",
+        "lambda:DeleteFunction",
+        "lambda:GetFunctionConfiguration",
+        "lambda:AddPermission",
+        "lambda:RemovePermission"
+      );
+      cdkDeployPolicy.addResources(
+        def.scope.formatArn({
+          service: "cloudformation",
+          resource: "stack",
+          resourceName: "CDKToolkit/*",
+        }),
+        def.scope.formatArn({
+          service: "cloudformation",
+          resource: "stack",
+          resourceName: `${lambdaApiStackName}/*`,
+        }),
+        def.scope.formatArn({
+          service: "lambda",
+          resource: "function",
+          sep: ":",
+          resourceName: lambdaFunctionName,
+        }),
+        "arn:aws:s3:::cdktoolkit-stagingbucket-*"
+      );
+      const editOrCreateLambdaDependencies = new PolicyStatement();
+      editOrCreateLambdaDependencies.addActions(
+        "iam:GetRole",
+        "iam:PassRole",
+        "iam:CreateRole",
+        "iam:AttachRolePolicy",
+        "iam:PutRolePolicy",
+        "apigateway:GET",
+        "apigateway:DELETE",
+        "apigateway:PUT",
+        "apigateway:POST",
+        "apigateway:PATCH",
+        "s3:CreateBucket",
+        "s3:PutBucketTagging"
+      );
+      editOrCreateLambdaDependencies.addResources("*");
+      project.addToRolePolicy(cdkDeployPolicy);
+      project.addToRolePolicy(editOrCreateLambdaDependencies);
 
-            const buildOutput = new Artifact(`BuildOutput`)
-            const buildAction = new CodeBuildAction({
-                actionName: `Build`,
-                project,
-                input: sourceOutput,
-                outputs: [buildOutput],
-            })
+      const buildOutput = new Artifact(`BuildOutput`);
+      const buildAction = new CodeBuildAction({
+        actionName: `Build`,
+        project,
+        input: sourceOutput,
+        outputs: [buildOutput],
+      });
 
-            pipeline.addStage({
-                stageName: "build",
-                actions: [buildAction],
-            })
+      pipeline.addStage({
+        stageName: "build",
+        actions: [buildAction],
+      });
 
-            return buildOutput
-        }
+      return buildOutput;
+    };
 
-        createBuildStage(pipeline, sourceOutput)
+    createBuildStage(pipeline, sourceOutput);
 
-        return { createBuildStage } as const
-    },
-    (props) => props
-)
+    return { createBuildStage } as const;
+  },
+  (props) => props
+);
--- typescript/lambda-api-ci/lib/lambda-api-stack.ts
+++ typescript/lambda-api-ci/lib/lambda-api-stack.ts
@@ -1,46 +1,46 @@
-import { LambdaIntegration, MethodLoggingLevel, RestApi } from "@aws-cdk/aws-apigateway"
-import { PolicyStatement } from "@aws-cdk/aws-iam"
-import { Function, Runtime, AssetCode } from "@aws-cdk/aws-lambda"
-import { Duration, Stack, StackProps } from "@aws-cdk/core"
+import {
+  LambdaIntegration,
+  MethodLoggingLevel,
+  RestApi,
+} from "@aws-cdk/aws-apigateway";
+import { PolicyStatement } from "@aws-cdk/aws-iam";
+import { Function, Runtime, AssetCode } from "@aws-cdk/aws-lambda";
+import { Duration, Stack, StackProps } from "@aws-cdk/core";
 import * as s3 from "@aws-cdk/aws-s3";
-import { C$ } from "@crosshatch/cdk"
+import { C$ } from "@crosshatch/cdk";
 
 interface LambdaApiStackProps extends StackProps {
-    functionName: string
+  functionName: string;
 }
 
-export const CDKExampleLambdaApiStack = C$(
-    Stack,
-    (def, props: LambdaApiStackProps) => {
-        const bucket = def`WidgetStore`(s3.Bucket)
+export const CDKExampleLambdaApiStack = C$(Stack, (def, props: LambdaApiStackProps) => {
+    const bucket = def`WidgetStore`(s3.Bucket);
 
-        const restApi = def`${def.scope.stackName}RestApi`(RestApi, {
-            deployOptions: {
-                stageName: "beta",
-                metricsEnabled: true,
-                loggingLevel: MethodLoggingLevel.INFO,
-                dataTraceEnabled: true,
-            },
-        })
+    const restApi = def`${def.scope.stackName}RestApi`(RestApi, {
+        deployOptions: {
+        stageName: "beta",
+        metricsEnabled: true,
+        loggingLevel: MethodLoggingLevel.INFO,
+        dataTraceEnabled: true,
+        },
+    });
 
-        const lambdaPolicy = new PolicyStatement()
-        lambdaPolicy.addActions("s3:ListBucket")
-        lambdaPolicy.addResources(bucket.bucketArn)
+    const lambdaPolicy = new PolicyStatement();
+    lambdaPolicy.addActions("s3:ListBucket");
+    lambdaPolicy.addResources(bucket.bucketArn);
 
-        const lambdaFunction = def`${props.functionName}`(Function, {
-            functionName: props.functionName,
-            handler: "handler.handler",
-            runtime: Runtime.NODEJS_10_X,
-            code: new AssetCode(`./src`),
-            memorySize: 512,
-            timeout: Duration.seconds(10),
-            environment: {
-                BUCKET: bucket.bucketName,
-            },
-            initialPolicy: [lambdaPolicy],
-        })
+    const lambdaFunction = def`${props.functionName}`(Function, {
+        functionName: props.functionName,
+        handler: "handler.handler",
+        runtime: Runtime.NODEJS_10_X,
+        code: new AssetCode(`./src`),
+        memorySize: 512,
+        timeout: Duration.seconds(10),
+        environment: {
+        BUCKET: bucket.bucketName,
+        },
+        initialPolicy: [lambdaPolicy],
+    });
 
-        restApi.root.addMethod("GET", new LambdaIntegration(lambdaFunction, {}))
-    },
-    (props) => props
-)
+    restApi.root.addMethod("GET", new LambdaIntegration(lambdaFunction, {}));
+}, (props) => props);
